357f4baae1a24f9e3bbb710b43734b2100cdba30
Allison Bhavsar <albhavsa@ucsd.edu>
2022-01-09 19:35:49 -0800

    css styles for active and inactive tabs

--- pages/league.tsx
+++ pages/league.tsx
@@ -3,16 +3,31 @@ import React, { useState } from 'react';
 import styles from '../styles/League.module.css';
 
 const League: NextPage = () => {
-
-  const [display, setDisplay] = useState('');
+  const [display, setDisplay] = useState<"Classes" | "Students" | "Staff">("Classes");
 
   return (
     <div className={styles.tabContainer}>
       <div className={styles.tabs}>
-        <button className={styles.tabLinks}>Classes</button>
-        <button className={styles.tabLinks}>Students</button>
-        <button className={styles.tabLinks}>Staff</button>
+        <button
+          className={display === "Classes" ? styles.activeTab : styles.inactiveTab}
+          onClick={() => setDisplay("Classes")}
+        >
+          Classes
+        </button>
+        <button
+          className={display === "Students" ? styles.activeTab : styles.inactiveTab}
+          onClick={() => setDisplay("Students")}
+        >
+          Students
+        </button>
+        <button
+          className={display === "Staff" ? styles.activeTab : styles.inactiveTab}
+          onClick={() => setDisplay("Staff")}
+        >
+          Staff
+        </button>
       </div>
+      {display}
     </div>
   )
 }

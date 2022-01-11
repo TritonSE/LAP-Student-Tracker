0a928d47431932b6a7d0b6d3f7caa90d6b871bf9
Allison Bhavsar <albhavsa@ucsd.edu>
2022-01-11 12:16:02 -0800

    ideas for rendering content (classes/students/staff)

--- pages/league.tsx
+++ pages/league.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '../styles/League.module.css';
 
 const allTabs = ['Classes', 'Students', 'Staff'] as const;
@@ -7,24 +7,40 @@ type Tab = (typeof allTabs)[number];
 
 const League: NextPage = () => {
   const [display, setDisplay] = useState<Tab>(allTabs[0]);
+  const [content, setContent] = useState<Record<Tab, string[]> | null>(null);
+
+  useEffect(
+    () => {
+      // eventually api call to get classes/students/staff...
+      setContent({
+        'Classes': ['Class 1', 'Class 2'],
+        'Students': ['Student 1', 'Student 2'],
+        'Staff': ['Staff 1', 'Staff 2'],
+      })
+    },
+    []);
 
   return (
-    <div className={styles.tabContainer}>
-      {/* Render Classes, Students, Staff tabs */}
-      <div className={styles.tabs}>
-        {allTabs.map((tabName) => {
-          return (
-            <button
-              className={display === tabName ? styles.activeTab : styles.inactiveTab}
-              onClick={() => setDisplay(tabName)}
-            >
-              {tabName}
-            </button>)
-        })}
+    <>
+      <div className={styles.tabContainer}>
+        {/* Render Classes, Students, Staff tabs */}
+        <div className={styles.tabs}>
+          {allTabs.map((tabName) => {
+            return (
+              <button
+                className={display === tabName ? styles.activeTab : styles.inactiveTab}
+                onClick={() => setDisplay(tabName)}
+              >
+                {tabName}
+              </button>)
+          })}
+        </div>
       </div>
 
-      {display}
-    </div>
+      <h1>{display}</h1>
+      {content ? content[display].map((item) => <li>{item}</li>) : null}
+    </>
+
   )
 }
 

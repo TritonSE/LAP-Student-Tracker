f731b7104807959d5a3e06fda0df0089a6914914
Allison Bhavsar <albhavsa@ucsd.edu>
2022-01-11 11:22:15 -0800

    Made tab rendering code less redundant

--- pages/league.tsx
+++ pages/league.tsx
@@ -2,31 +2,27 @@ import type { NextPage } from 'next';
 import React, { useState } from 'react';
 import styles from '../styles/League.module.css';
 
+const allTabs = ['Classes', 'Students', 'Staff'] as const;
+type Tab = (typeof allTabs)[number];
+
 const League: NextPage = () => {
-  const [display, setDisplay] = useState<"Classes" | "Students" | "Staff">("Classes");
+  const [display, setDisplay] = useState<Tab>(allTabs[0]);
 
   return (
     <div className={styles.tabContainer}>
+      {/* Render Classes, Students, Staff tabs */}
       <div className={styles.tabs}>
-        <button
-          className={display === "Classes" ? styles.activeTab : styles.inactiveTab}
-          onClick={() => setDisplay("Classes")}
-        >
-          Classes
-        </button>
-        <button
-          className={display === "Students" ? styles.activeTab : styles.inactiveTab}
-          onClick={() => setDisplay("Students")}
-        >
-          Students
-        </button>
-        <button
-          className={display === "Staff" ? styles.activeTab : styles.inactiveTab}
-          onClick={() => setDisplay("Staff")}
-        >
-          Staff
-        </button>
+        {allTabs.map((tabName) => {
+          return (
+            <button
+              className={display === tabName ? styles.activeTab : styles.inactiveTab}
+              onClick={() => setDisplay(tabName)}
+            >
+              {tabName}
+            </button>)
+        })}
       </div>
+
       {display}
     </div>
   )

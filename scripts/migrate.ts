fb69510a5d8ac6f26064bb6be1808db131d47fe2
Anshul-Birla <42627151+Anshul-Birla@users.noreply.github.com>
2022-01-12 12:12:38 -0800

    Add Linting and CI to Repo (#39)
    * linting setup
    
    * added workflow file
    
    * PR template
    
    * added docker
    
    * updated readme
    
    * lint fix
    
    * added migration command
    
    * lint fix
    
    * migration fix
    
    * small commit to fix postgres issue

---
+++ scripts/migrate.ts
@@ -0,0 +1,21 @@
+const { Pool } = require("pg");
+const { migrate } = require("postgres-migrations");
+
+const client = new Pool({
+  user: process.env.DB_USER || "postgres",
+  host: process.env.DB_HOST || "localhost",
+  database: "postgres",
+  password: process.env.DB_PASS || "postgres",
+  port: 5432,
+});
+
+const runMigration = async () => {
+  try {
+    await migrate({ client }, "./migrations");
+    console.log("Migration Ran Succesfully");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+runMigration().then(() => process.exit());

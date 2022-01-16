48d46d2d865bfa317b60aaf6e9d7cd4aa4bc7182
Nishant <nishantb1130@gmail.com>
2022-01-16 13:44:33 -0800

    initial outline

---
+++ pages/api/users.ts
@@ -0,0 +1,23 @@
+
+
+// POST new user to database
+
+// GET user based on id
+
+// GET list of all users
+
+// GET list of all users with a certain role
+
+// PUT new user information given an id
+
+
+// import { object, string, TypeOf } from "yup"; 
+
+// export const userScheme = object({
+//     id: string().ensure().required(),
+//     firstName: string().ensure().required(),
+//     lastName: string().ensure().required(),
+//     email: string().ensure().required(),
+//     role: string().ensure().required(), // parent, volunteer, student, admin, teacher
+//     phoneNumber: string().optional(),    
+// })

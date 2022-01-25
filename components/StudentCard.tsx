7d79e7bcba64dd6e14588dc6db8c5e23ba43742d
allisonbhavsar <47436434+allisonbhavsar@users.noreply.github.com>
2022-01-24 17:35:30 -0800

    Feature/ryanbuui/7 classes students staff frontend (#44)
    * Added basic tab structure and styling
    
    * css styles for active and inactive tabs
    
    * Made tab rendering code less redundant
    
    * ideas for rendering content (classes/students/staff)
    
    * global vars for font colors
    
    * Added basic title and search bar with styling
    
    * Fixed list key warning
    
    * Added basic list for classes, students, staff components
    
    * Added filters and updated styling
    
    * card components in progress
    
    * rendered class times via moment.js
    
    * added classes to student schema and improved css styling
    
    * fixed yup schema issues and moved global styles to globals.css
    
    * fixed requested changes
    
    * fixed class schema levels and changed card props
    
    * lint fix
    
    Co-authored-by: ryanbuui <ryanbui.76@gmail.com>

---
+++ components/StudentCard.tsx
@@ -0,0 +1,21 @@
+import styles from "../styles/Components.module.css";
+
+type StudentCardProps = {
+  firstName: string;
+  lastName: string;
+  level: number;
+  classes: string[];
+};
+
+const StudentCard: React.FC<StudentCardProps> = ({ firstName, lastName, level, classes }) => {
+  return (
+    <div className={styles.listElem}>
+      <div>
+        <p className={styles.leftText}>{firstName + " " + lastName}</p>
+      </div>
+      <div className={styles.rightText}>{["Level " + level, "•", classes[0]].join(" ")}</div>
+    </div>
+  );
+};
+
+export default StudentCard;

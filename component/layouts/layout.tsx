46d9f14163e7d75ed7beea021dd9adee040b842e
Anshul-Birla <anshulbirla@gmail.com>
2022-01-26 16:08:10 -0800

    fixed navbar changes

--- component/layouts/layout.tsx
+++ component/layouts/layout.tsx
@@ -1,17 +1,16 @@
 import { useRouter } from "next/router";
 import { Navbar } from "../navbar"
-import type { NextPage } from 'next'
 import React, { FC } from "react";
 
 
-const Layout:React.FC<NextPage> = ({children}) => {
+const Layout: React.FC = ({ children }) => {
     const router = useRouter();
     const hideNavbar = ['/login', '/signup', '/home'];
     const showNavbar = hideNavbar.includes(router.pathname) ? false : true;
     return (
         <div>
-        {showNavbar && <Navbar />}
-        {children}
+            {showNavbar && <Navbar />}
+            {children}
         </div>
     );
 }
--- component/navbar.tsx
+++ component/navbar.tsx
@@ -1,39 +1,38 @@
 import styles from "../styles/Navbar.module.css";
-import type { NextPage } from 'next'
 import React from "react";
-export const Navbar: React.FC<NextPage> = ({children})=>{
-    return <> 
-          <header className={styles.header}>
-        <nav className={styles.navbar}>
-            <a className={styles.navlogo}><img src="logo1.png"></img><img src="logo2.png"></img></a>
+export const Navbar: React.FC = ({ children }) => {
+  return <>
+    <header className={styles.header}>
+      <nav className={styles.navbar}>
+        <a className={styles.navlogo}><img src="logo1.png"></img><img src="logo2.png"></img></a>
 
-          <ul className={styles.navmenu}>
-            <li className={styles.navitem}>
-                <a 
-                href= "home"
-                className={styles.navlink}>Home</a>
-            </li>
-            <li className={styles.navitem}>
-                <a 
-                href= "league"
-                className={styles.navlink}>The League</a>
-       
-            </li>
-            <li className={styles.navitem}>
-                <a 
-                href= "profile"
-                className={styles.navlink}>Profile</a>
-      
-            </li>
-          </ul>
-          <button className={styles.none}>
-            <span className={styles.bar}></span>
-            <span className={styles.bar}></span>
-            <span className={styles.bar}></span>
-          </button>
-        </nav>
-      </header>
+        <ul className={styles.navmenu}>
+          <li className={styles.navitem}>
+            <a
+              href="home"
+              className={styles.navlink}>Home</a>
+          </li>
+          <li className={styles.navitem}>
+            <a
+              href="league"
+              className={styles.navlink}>The League</a>
+
+          </li>
+          <li className={styles.navitem}>
+            <a
+              href="profile"
+              className={styles.navlink}>Profile</a>
+
+          </li>
+        </ul>
+        <button className={styles.none}>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </button>
+      </nav>
+    </header>
     {children}
-    
-    </> 
+
+  </>
 }

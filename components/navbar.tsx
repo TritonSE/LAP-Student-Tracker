656f48a88ad6f52c81942f435f49d14ae2ae9fe3
zainnk902 <68146410+zainnk902@users.noreply.github.com>
2022-01-31 05:28:25 -0800

    fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,8 @@
 import styles from "../styles/Navbar.module.css";
 import React from "react";
+import { useRouter } from "next/router";
 export const Navbar: React.FC = ({ children }) => {
+  const router = useRouter();
   return (
     <>
       <header className={styles.header}>
@@ -9,32 +11,43 @@ export const Navbar: React.FC = ({ children }) => {
             <img src="logo1.png"></img>
             <img src="logo2.png"></img>
           </a>
-
-          <ul className={styles.navmenu}>
-            <li className={styles.navitem}>
-              <a
-                /* 
+          <div>
+            <div>
+              <ul className={styles.navmenu}>
+                <li className={styles.navitem}>
+                  <a
+                    /* 
             These are the 3 navbar buttons, we may want to add some conditional 
             statements here to say when the buttons are displayed and not, like in
             the signup page, where they shouldn't be displayed
             */
-                href="home"
-                className={styles.navlink}
-              >
-                Home
-              </a>
-            </li>
-            <li className={styles.navitem}>
-              <a href="league" className={styles.navlink}>
-                The League
-              </a>
-            </li>
-            <li className={styles.navitem}>
-              <a href="profile" className={styles.navlink}>
-                Profile
-              </a>
-            </li>
-          </ul>
+
+                    href="home"
+                    className={router.pathname == "/home" ? styles.clicked : styles.navlink}
+                  >
+                    Home
+                  </a>
+                </li>
+
+                <li className={styles.navitem}>
+                  <a
+                    href="league"
+                    className={router.pathname == "/league" ? styles.clicked : styles.navlink}
+                  >
+                    The League
+                  </a>
+                </li>
+                <li className={styles.navitem}>
+                  <a
+                    href="profile"
+                    className={router.pathname == "/profile" ? styles.clicked : styles.navlink}
+                  >
+                    Profile
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
           <button className={styles.none}>
             <span className={styles.bar}></span>
             <span className={styles.bar}></span>

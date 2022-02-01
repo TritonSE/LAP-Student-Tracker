ef0bc89b762076d35e00ad4f800a2293a51ae29d
Anshul-Birla <anshulbirla@gmail.com>
2022-01-31 19:06:02 -0800

    fixes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,56 +5,49 @@ export const Navbar: React.FC = ({ children }) => {
   const router = useRouter();
   return (
     <>
-      <header className={styles.header}>
-        <nav className={styles.navbar}>
-          <a className={styles.navlogo}>
-            <img src="logo1.png"></img>
-            <img src="logo2.png"></img>
-          </a>
+      <nav className={styles.navbar}>
+        <a className={styles.navlogo}>
+          <img src="logo1.png"></img>
+          <img src="logo2.png"></img>
+        </a>
+        <div>
           <div>
-            <div>
-              <ul className={styles.navmenu}>
-                <li className={styles.navitem}>
-                  <a
-                    /* 
-            These are the 3 navbar buttons, we may want to add some conditional 
-            statements here to say when the buttons are displayed and not, like in
-            the signup page, where they shouldn't be displayed
-            */
+            <ul className={styles.navmenu}>
+              <li className={styles.navitem}>
+                <a
+                  /* 
+          These are the 3 navbar buttons, we may want to add some conditional 
+          statements here to say when the buttons are displayed and not, like in
+          the signup page, where they shouldn't be displayed
+          */
 
-                    href="home"
-                    className={router.pathname == "/home" ? styles.clicked : styles.navlink}
-                  >
-                    Home
-                  </a>
-                </li>
+                  href="home"
+                  className={router.pathname == "/home" ? styles.clicked : styles.navlink}
+                >
+                  Home
+                </a>
+              </li>
 
-                <li className={styles.navitem}>
-                  <a
-                    href="league"
-                    className={router.pathname == "/league" ? styles.clicked : styles.navlink}
-                  >
-                    The League
-                  </a>
-                </li>
-                <li className={styles.navitem}>
-                  <a
-                    href="profile"
-                    className={router.pathname == "/profile" ? styles.clicked : styles.navlink}
-                  >
-                    Profile
-                  </a>
-                </li>
-              </ul>
-            </div>
+              <li className={styles.navitem}>
+                <a
+                  href="league"
+                  className={router.pathname == "/league" ? styles.clicked : styles.navlink}
+                >
+                  The League
+                </a>
+              </li>
+              <li className={styles.navitem}>
+                <a
+                  href="profile"
+                  className={router.pathname == "/profile" ? styles.clicked : styles.navlink}
+                >
+                  Profile
+                </a>
+              </li>
+            </ul>
           </div>
-          <button className={styles.none}>
-            <span className={styles.bar}></span>
-            <span className={styles.bar}></span>
-            <span className={styles.bar}></span>
-          </button>
-        </nav>
-      </header>
+        </div>
+      </nav>
       {children}
     </>
   );
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,4 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 //This is the page that is rendered when the 'Home' button from the Navbar is clicked
 const Home: NextPage = () => {
   return <h1>HOME</h1>;

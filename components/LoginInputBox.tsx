8d6d216ad492c74a04fe6c58a7b115db07b4808d
anvitaa22 <asekhsar@ucsd.edu>
2022-02-19 13:54:16 -0800

    login branch with most components

---
+++ components/LoginInputBox.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import TextField from '@mui/material/TextField';
+
+const InputBox: React.FC<{ text: string }> = ({ text }) => {
+
+    let title: string
+    let type: string
+
+    title = text[0].toUpperCase() + text.substring(1); //capatalize first letter of text
+
+    if (text == "password"){
+        type = "password"
+    }
+    else{
+        type = "text"
+    }
+
+    return (
+        <TextField
+        id="filled-basic" 
+        label={title}
+        variant="filled"
+        type={type}
+        color="warning"
+        //value={email}
+        //onChange={handleChange}
+        InputProps = {{
+            disableUnderline: true,
+        }}
+        sx = {{
+            color: "black",
+            width: 690,
+            height: 80,
+            '& .MuiFilledInput-input': {
+            color: "#000000"
+            },
+            '& .MuiFormLabel-root': {
+                color: "#494C4E"
+                },
+        }}
+        />
+    )
+
+};
+
+export default InputBox;
---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+import React, { useState, useEffect } from "react";
+import styles from "../styles/Login.module.css";
+import { LoginPageMain, LoginNameInput, LoginPositionInput } from "../components/LoginViews";
+
+const Login: NextPage = () => {
+    
+    return <LoginPositionInput />;
+}
+
+export default Login;

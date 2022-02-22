c99bc368ad6294be591dc1fd87a673a3f195c458
Anshul-Birla <anshulbirla@gmail.com>
2022-02-22 15:12:49 -0800

    working on auth

---
+++ context/AuthContext.tsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useState, useMemo, useEffect } from 'react'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import { Navbar } from '../components/Navbar';
+import { User } from '../models/users';
+
+type AuthState = {
+  user: User | null,
+  loggedIn: boolean,
+  error: Error | null,
+  login: (email: string, password: string, rememberMe: boolean) => void,
+  logout: () => void,
+  signup: (firstName: string, lastName: string, email: string, password: string) => void
+  clearError: () => void
+}
+
+const init: AuthState = {
+  user: null,
+  loggedIn: false,
+  error: null,
+  login: () => { },
+  logout: () => { },
+  signup: () => { },
+}
+
+export const AuthContext = createContext<AuthState>(init);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const api = useContext(APIContext);
+
+  const
+}

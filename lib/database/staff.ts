3291a92d6c0768f1b09c1812463b5233e4ad83f7
Nishant Balaji <nishantb1130@gmail.com>
2022-03-08 15:47:56 -0800

    initialize refactor of staff

--- lib/database/staff.ts
+++ lib/database/staff.ts
@@ -1,10 +1,14 @@
 import { client } from "../db";
 import { User, UserSchema } from "../../models/users";
+import { Staff, StaffSchema } from "../../models/staff";
 import { array, TypeOf } from "io-ts";
 import { decode } from "io-ts-promise";
 
 const UserArraySchema = array(UserSchema);
 type userArrayType = TypeOf<typeof UserArraySchema>;
+
+const StaffArraySchema = array(StaffSchema);
+type staffArrayType = TypeOf<typeof StaffArraySchema>;
 
 // gets all staff in the database
 const getAllStaff = async (): Promise<User[]> => {
@@ -24,5 +28,14 @@ const getAllStaff = async (): Promise<User[]> => {
 
   return staffArray;
 };
+
+const getAllStaff2 = async (): Promise<Staff[]> => {
+  const query = {
+    text: "SELECT id, minLevel, maxLevel FROM users WHERE role = 'Teacher'",
+  };
+  const res = await client.query(query);
+
+  let teacherArray: staffArrayType;
+}
 
 export { getAllStaff };

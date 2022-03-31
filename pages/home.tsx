49c4d8a9142edbd054c91a0b1d41a24ff1816c09
Allison Bhavsar <albhavsa@ucsd.edu>
2022-03-31 09:56:10 -0700

    conditionally render home page based on auth context

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AdminHomeView } from "../components/Home/AdminHomeView";
+import { Error } from "../components/util/Error";
+import { AuthContext } from "../context/AuthContext";
 import { NextApplicationPage } from "./_app";
 //This is the page that is rendered when the 'Home' button from the Navbar is clicked
 const Home: NextApplicationPage = () => {
-  return <AdminHomeView />;
+  const { user } = useContext(AuthContext);
+  if (user == null) return <Error />;
+  const pageToRender = user.role == "Admin" ? <AdminHomeView /> : <div>Home</div>;
+  return pageToRender;
 };
 
 Home.requireAuth = true;

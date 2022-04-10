3cde96d79629c01b8e9af2ebc61ca90a87813aa3
Nishant Balaji <nishantb1130@gmail.com>
2022-04-09 17:46:20 -0700

    lint

--- lib/database/staff.ts
+++ lib/database/staff.ts
@@ -8,11 +8,12 @@ type staffArrayType = TypeOf<typeof StaffArraySchema>;
 
 const getAllStaff = async (): Promise<Staff[]> => {
   const query = {
-    text: "SELECT users.id, users.first_name, users.last_name, users.email, users.role, "
-    + "users.phone_number, users.address, classes.min_level, classes.max_level, classes.language " 
-    + "FROM (((event_information INNER JOIN classes ON event_information.id = classes.event_information_id) " 
-    + "INNER JOIN commitments ON commitments.event_information_id = event_information.id) " 
-    + "FULL OUTER JOIN users ON commitments.user_id = users.id) WHERE role = 'Teacher' OR role = 'Staff' OR role = 'Admin';",
+    text:
+      "SELECT users.id, users.first_name, users.last_name, users.email, users.role, " +
+      "users.phone_number, users.address, classes.min_level, classes.max_level, classes.language " +
+      "FROM (((event_information INNER JOIN classes ON event_information.id = classes.event_information_id) " +
+      "INNER JOIN commitments ON commitments.event_information_id = event_information.id) " +
+      "FULL OUTER JOIN users ON commitments.user_id = users.id) WHERE role = 'Teacher' OR role = 'Staff' OR role = 'Admin';",
   };
 
   const res = await client.query(query);

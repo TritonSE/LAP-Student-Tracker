81718ad4ce642e7fb5570da583a0cc0e73c15dd2
Nishant Balaji <nishantb1130@gmail.com>
2022-04-09 17:45:29 -0700

    remove old getAllStaff method

--- lib/database/staff.ts
+++ lib/database/staff.ts
@@ -10,37 +10,6 @@ type userArrayType = TypeOf<typeof UserArraySchema>;
 const StaffArraySchema = array(StaffSchema);
 type staffArrayType = TypeOf<typeof StaffArraySchema>;
 
-// interface StaffMapType {
-//   id: number;
-//   [index: number]: number;
-// }
-
-// {
-//   user_id: {
-//     classes: [],
-//     languages: [],
-//   }
-// }
-
-// // gets all staff in the database
-// const getAllStaff = async (): Promise<User[]> => {
-//   const query = {
-//     text: "SELECT id, first_name, last_name, email, role, phone_number, address FROM users WHERE role = 'Teacher' OR role = 'Admin'",
-//   };
-
-//   const res = await client.query(query);
-
-//   let staffArray: userArrayType;
-
-//   try {
-//     staffArray = await decode(UserArraySchema, res.rows);
-//   } catch (e) {
-//     throw Error("Fields returned incorrectly from database");
-//   }
-
-//   return staffArray;
-// };
-
 const getAllStaff = async (): Promise<Staff[]> => {
   const query = {
     text: "SELECT users.id, users.first_name, users.last_name, users.email, users.role, "
@@ -66,4 +35,3 @@ const getAllStaff = async (): Promise<Staff[]> => {
 };
 
 export { getAllStaff };
-// , getTeachers

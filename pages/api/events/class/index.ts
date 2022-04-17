b2bc86915bd7d0c8a4e713acfdaa3f603357efcc
allisonbhavsar <47436434+allisonbhavsar@users.noreply.github.com>
2022-04-17 12:22:52 -0700

    Feature/allisonbhavsar/12 class check teacher availability (#74)

--- pages/api/events/class/index.ts
+++ pages/api/events/class/index.ts
@@ -1,12 +1,18 @@
 import { NextApiHandler, NextApiRequest, NextApiResponse } from "next";
-import { createClassEvent, NonExistingTeacher } from "../../../../lib/database/events";
+import {
+  createClassEvent,
+  teachersExist,
+  NonExistingTeacher,
+  TeacherConflictError,
+  validateTimes,
+} from "../../../../lib/database/events";
 import { createCalendarEvent } from "../../../../lib/database/calendar";
 import { createCommitment } from "../../../../lib/database/commitments";
 import { CreateClassEvent, ClassEvent, CreateClassEventSchema } from "../../../../models/events";
 import { decode } from "io-ts-promise";
 import { StatusCodes } from "http-status-codes";
 import { rrulestr } from "rrule";
-import { DateTime } from "luxon";
+import { Interval, DateTime } from "luxon";
 
 // handles requests to /api/events/class
 const eventHandler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -20,12 +26,8 @@ const eventHandler: NextApiHandler = async (req: NextApiRequest, res: NextApiRes
           return res.status(StatusCodes.BAD_REQUEST).json("Fields are not correctly entered");
         }
         try {
-          const result = await createClassEvent(
-            newEvent.name,
-            newEvent.neverEnding,
-            newEvent.backgroundColor,
-            newEvent.teachers
-          );
+          // verify the teachers exist in the database
+          const teachers = await teachersExist(newEvent.teachers);
 
           const ruleObj = rrulestr(newEvent.rrule);
           const initialDate = ruleObj.all()[0];
@@ -35,6 +37,9 @@ const eventHandler: NextApiHandler = async (req: NextApiRequest, res: NextApiRes
           const allDates = newEvent.neverEnding
             ? ruleObj.between(initialDate, yearInAdvanceDate)
             : ruleObj.all();
+
+          // allDates.forEach((d) => { console.log(d) })
+
           const startTime = DateTime.fromFormat(newEvent.startTime, "HH:mm", {
             zone: newEvent.timeZone,
           });
@@ -42,40 +47,69 @@ const eventHandler: NextApiHandler = async (req: NextApiRequest, res: NextApiRes
             zone: newEvent.timeZone,
           });
 
-          // Loops through all dates and inserts into calender_information table
+          const intervals: Interval[] = [];
+
+          // Create start-end interval from each date
           for (const date of allDates) {
             const dateWithoutTime = new Date(date.getFullYear(), date.getMonth(), date.getDate());
 
-            const dateStart = DateTime.fromJSDate(dateWithoutTime).set({
-              hour: startTime.hour,
-              minute: startTime.minute,
-              second: startTime.second,
-            });
+            const dateStart = DateTime.fromJSDate(dateWithoutTime)
+              .set({
+                hour: startTime.hour,
+                minute: startTime.minute,
+                second: startTime.second,
+              })
+              .setZone(newEvent.timeZone);
 
-            const dateEnd = DateTime.fromJSDate(dateWithoutTime, { zone: newEvent.timeZone }).set({
-              hour: endTime.hour,
-              minute: endTime.minute,
-              second: endTime.second,
-            });
+            const dateEnd = DateTime.fromJSDate(dateWithoutTime)
+              .set({
+                hour: endTime.hour,
+                minute: endTime.minute,
+                second: endTime.second,
+              })
+              .setZone(newEvent.timeZone);
 
-            try {
-              await createCalendarEvent(result.classEventId, dateStart.toISO(), dateEnd.toISO());
-            } catch (e) {
-              return res.status(StatusCodes.BAD_REQUEST).json("Calender information is incorrect");
+            intervals.push(Interval.fromDateTimes(dateStart, dateEnd));
+          }
+
+          // verify that each teacher is available during class times
+          try {
+            for (const teacher of teachers) {
+              await validateTimes(teacher, intervals);
             }
+          } catch (e) {
+            if (e instanceof TeacherConflictError)
+              return res.status(StatusCodes.BAD_REQUEST).json(e.message);
+            else res.status(StatusCodes.INTERNAL_SERVER_ERROR).json("Internal server error");
+          }
+
+          // create the class event in event_information table
+          const result = await createClassEvent(
+            newEvent.name,
+            newEvent.neverEnding,
+            newEvent.backgroundColor
+          );
+
+          // insert all date intervals into calendar_information table
+          try {
+            for (const interval of intervals) {
+              await createCalendarEvent(result, interval.start.toISO(), interval.end.toISO());
+            }
+          } catch (e) {
+            return res.status(StatusCodes.BAD_REQUEST).json("Calendar information is incorrect");
           }
 
           // Loops through teachers and inserts into commitments table
           try {
-            for (const teacher of result.teacherIds) {
-              await createCommitment(teacher, result.classEventId);
+            for (const teacher of teachers) {
+              await createCommitment(teacher.id, result);
             }
           } catch (e) {
             return res.status(StatusCodes.BAD_REQUEST).json("Commitment information is incorrect");
           }
 
           const responseBody: ClassEvent = {
-            eventInformationId: result.classEventId,
+            eventInformationId: result,
             startTime: startTime.toISOTime(),
             endTime: endTime.toISOTime(),
             timeZone: newEvent.timeZone,

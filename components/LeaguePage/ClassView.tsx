517d55561a30ec4a07d03527a85878618866db0f
Nishant Balaji <nishantb1130@gmail.com>
2022-04-18 00:58:18 -0700

    filter by search, working radio buttons and checkboxes

--- components/LeaguePage/ClassView.tsx
+++ components/LeaguePage/ClassView.tsx
@@ -1,3 +1,4 @@
+import { Input } from "@mui/material";
 import React, { useState } from "react";
 import { Class } from "../../models/class";
 import { ClassCard } from "./ClassCard";
@@ -20,54 +21,80 @@ const filters = [
 ];
 
 const ClassView: React.FC<ClassViewProp> = ({ classes }) => {
-
   const [searchBox, setSearchBox] = useState("");
-  const [orderBy, setOrderBy] = useState("");
-  const [selectedClassLevels, setSelectedClassLevels] = useState("");
+  const [orderBy, setOrderBy] = useState({alpha: false, level: false});
+  const [selectedClassLevels, setSelectedClassLevels] = useState({});
 
+  const onSearchInput = (event: any) => {
+    setSearchBox(event.target.value);
+  };
+
+  const changeSort = (event: any) => {
+    if (event.target.value === "alpha") {
+      setOrderBy({alpha: true, level: false});
+    }
+    if (event.target.value === "level") {
+      setOrderBy({alpha: false, level: true});
+    }
+  };
+
+  const onFilterCheck = (event: any) => {
+    const newSelectedClassLevels: any = { ...selectedClassLevels };
+    newSelectedClassLevels[event.target.value] = event.target.checked;
+    setSelectedClassLevels(newSelectedClassLevels);
+    console.log(selectedClassLevels);
+  };
+  
   return (
-  <div className={styles.compContainer}>
-    <div className={styles.leftContainer}>
-      <h1 className={styles.compTitle}>Classes</h1>
-      <div className={styles.compList}>
-        <ul className={styles.scroll}>
-          {classes.map((tempClass) => (
-            <ClassCard
-              key={tempClass.eventInformationId}
-              name={tempClass.name}
-              minLevel={tempClass.minLevel}
-              maxLevel={tempClass.maxLevel}
-              rrstring={tempClass.rrstring}
-              startTime={tempClass.startTime}
-              endTime={tempClass.endTime}
-            />
+    <div className={styles.compContainer}>
+      <div className={styles.leftContainer}>
+        <h1 className={styles.compTitle}>Classes</h1>
+        <div className={styles.compList}>
+          <ul className={styles.scroll}>
+            {(classes.filter((tempClass) => 
+              tempClass.name.includes(searchBox))).map((tempClass) => 
+              <ClassCard
+                key={tempClass.eventInformationId}
+                name={tempClass.name}
+                minLevel={tempClass.minLevel}
+                maxLevel={tempClass.maxLevel}
+                rrstring={tempClass.rrstring}
+                startTime={tempClass.startTime}
+                endTime={tempClass.endTime}
+              />
+            )}
+          </ul>
+        </div>
+      </div>
+      <span className={styles.spacer} />
+      <div className={styles.rightContainer}>
+        <input
+          type="text"
+          placeholder="Search classes"
+          className={styles.searchBar}
+          onChange={onSearchInput}
+        ></input>
+        <h2 className={styles.orderTitle}>Order By:</h2>
+        <div className={styles.orderElem}>
+          <p className={styles.listItemText}>Alphabetical</p>
+          <input type="radio" value="alpha" checked={orderBy.alpha} onChange={changeSort}></input>
+        </div>
+        <div className={styles.orderElem}>
+          <p className={styles.listItemText}>Level</p>
+          <input type="radio" value="level" checked={orderBy.level} onChange={changeSort}></input>
+        </div>
+        <h2 className={styles.filterTitle}>Filter By:</h2>
+        <ul className={styles.filterList}>
+          {filters.map((l) => (
+            <li key={l}>
+              <p className={styles.listItemText}>{l}</p>
+              <input type="checkbox" value={l} onClick={onFilterCheck}></input>
+            </li>
           ))}
         </ul>
       </div>
     </div>
-    <span className={styles.spacer} />
-    <div className={styles.rightContainer}>
-      <input type="text" placeholder="Search classes" className={styles.searchBar}></input>
-      <h2 className={styles.orderTitle}>Order By:</h2>
-      <div className={styles.orderElem}>
-        <p className={styles.listItemText}>Alphabetical</p>
-        <input type="radio"></input>
-      </div>
-      <div className={styles.orderElem}>
-        <p className={styles.listItemText}>Level</p>
-        <input type="radio"></input>
-      </div>
-      <h2 className={styles.filterTitle}>Filter By:</h2>
-      <ul className={styles.filterList}>
-        {filters.map((l) => (
-          <li key={l}>
-            <p className={styles.listItemText}>{l}</p>
-            <input type="checkbox"></input>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </div>
-)};
+  );
+};
 
 export { ClassView };

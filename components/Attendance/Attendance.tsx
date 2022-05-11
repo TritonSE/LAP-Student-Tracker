ce2451fd97d32bcd2cc92a67f57cac8af33c64e9
Nishant Balaji <nishantb1130@gmail.com>
2022-05-10 19:49:09 -0700

    basic format

--- components/Attendance/Attendance.tsx
+++ components/Attendance/Attendance.tsx
@@ -3,8 +3,10 @@ import styles from './attendance.module.css';
 
 export const Attendance: React.FC = () => {
     return (
-        <div>
-            Attendance
+        <div className={styles.container}>
+            <div className={styles.title}>
+                Attendance
+            </div>
         </div>
     )
 }
--- components/ModuleView/Module.tsx
+++ components/ModuleView/Module.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import styles from './module.module.css';
+import styles from './modules.module.css';
 
 export const Module: React.FC = () => {
     return (
-        <div>
-            Module
+        <div className={styles.container}>
+            <div className={styles.title}>
+                Module
+            </div>
         </div>
     )
 }

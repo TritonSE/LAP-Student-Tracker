b6a2b1e6adf3ccaad2d4be461a20eef6ccdfb3ab
ryanbuui <ryanbui.76@gmail.com>
2022-05-25 04:55:01 -0700

    Updated and styled unapproved page

--- pages/unapproved.tsx
+++ pages/unapproved.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useRouter } from "next/router";
+import styles from "../styles/Unapproved.module.css";
 
 const Unapproved: React.FC = () => {
-  return <div>This account has not yet been approved. Please contact an admin for assistance.</div>;
+  const { logout } = useContext(AuthContext);
+  const router = useRouter();
+
+  const returnToSignUp = (): void => {
+    logout();
+    router.push("/login");
+  };
+
+  return (
+    <div className={styles.unapprovedContainer}>
+      <div className={styles.textContainer}>
+        <h1>We are verifying your account.</h1>
+        <p>Please hold tight until we review your credentials.</p>
+        <p>Have any questions? Please email admin@jointheleague.org</p>
+      </div>
+      <div className={styles.returnBtnContainer}>
+        <button className={styles.returnBtn} onClick={() => returnToSignUp()}>
+          Return to Sign up
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Unapproved;

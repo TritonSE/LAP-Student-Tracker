aa9a638c49415b6ed95d97ab8314274ff843cfc1
Allison Bhavsar <albhavsa@ucsd.edu>
2022-06-02 17:45:03 -0700

    made validateAvailabilities separate function and added comments

--- pages/api/events/class/index.ts
+++ pages/api/events/class/index.ts
@@ -6,8 +6,10 @@ import {
   teachersExist,
   validateTimes,
 } from "../../../../lib/database/events";
-import { getAvailabilityById } from "../../../../lib/database/availability";
-import { timeDateZoneToDateTime } from "../../../../lib/database/availability-feed";
+import {
+  TeacherAvailabilityError,
+  validateAvailabilities,
+} from "../../../../lib/database/availability";
 import { createCalendarEvent } from "../../../../lib/database/calendar";
 import { createCommitment } from "../../../../lib/database/commitments";
 import { ClassEvent, CreateClassEvent, CreateClassEventSchema } from "../../../../models/events";
@@ -16,23 +18,6 @@ import { StatusCodes } from "http-status-codes";
 import { rrulestr } from "rrule";
 import { DateTime, Interval } from "luxon";
 import { withAuth } from "../../../../middleware/withAuth";
-
-type Weekdays = "mon" | "tue" | "wed" | "thu" | "fri" | "sat" | "sun";
-type WeekdayIntervals = {
-  mon: Interval | null;
-  tue: Interval | null;
-  wed: Interval | null;
-  thu: Interval | null;
-  fri: Interval | null;
-  sat: Interval | null;
-};
-
-class TeacherAvailabilityError extends Error {
-  constructor(msg: string) {
-    super(msg);
-    Object.setPrototypeOf(this, TeacherAvailabilityError.prototype);
-  }
-}
 
 // handles requests to /api/events/class
 const classEventHandler: NextApiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -65,17 +50,6 @@ const classEventHandler: NextApiHandler = async (req: NextApiRequest, res: NextA
         });
 
         const intervals: Interval[] = [];
-        const intervalsByWeekDay: WeekdayIntervals = {
-          mon: null,
-          tue: null,
-          wed: null,
-          thu: null,
-          fri: null,
-          sat: null,
-        };
-
-        const indexToWeekdays = ["temp", "mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-        const seenWeekDays = new Set();
 
         // Create start-end interval from each date
         for (const date of allDates) {
@@ -97,17 +71,7 @@ const classEventHandler: NextApiHandler = async (req: NextApiRequest, res: NextA
             })
             .setZone(newEvent.timeZone, { keepLocalTime: true });
 
-          const interval = Interval.fromDateTimes(dateStart, dateEnd);
-          intervals.push(interval);
-
-          const weekDay = dateStart.weekday;
-          if (!seenWeekDays.has(weekDay)) {
-            seenWeekDays.add(weekDay);
-            const weekDayStr = indexToWeekdays[weekDay] as Weekdays;
-            if (weekDayStr != "sun") {
-              intervalsByWeekDay[weekDayStr] = interval;
-            }
-          }
+          intervals.push(Interval.fromDateTimes(dateStart, dateEnd));
         }
 
         // verify scheduling for each teacher
@@ -118,42 +82,7 @@ const classEventHandler: NextApiHandler = async (req: NextApiRequest, res: NextA
 
             // verify that teacher has availability set during this class time
             if (newEvent.checkAvailabilities) {
-              const availabilities = await getAvailabilityById(teacher.id);
-              let weekday: keyof WeekdayIntervals;
-              for (weekday in intervalsByWeekDay) {
-                const interval = intervalsByWeekDay[weekday];
-                let isAvailable = false;
-                if (availabilities && interval) {
-                  const availabilityTimes = availabilities[weekday];
-                  if (availabilityTimes) {
-                    for (const time of availabilityTimes) {
-                      const availableStartDateTime = timeDateZoneToDateTime(
-                        time[0],
-                        interval.start,
-                        availabilities.timeZone
-                      );
-                      const availableEndDateTime = timeDateZoneToDateTime(
-                        time[1],
-                        interval.start,
-                        availabilities.timeZone
-                      );
-                      const availabilityInterval = Interval.fromDateTimes(
-                        availableStartDateTime,
-                        availableEndDateTime
-                      );
-                      if (availabilityInterval.engulfs(interval)) {
-                        isAvailable = true;
-                        break;
-                      }
-                    }
-                  }
-                }
-                if (interval && !isAvailable) {
-                  throw new TeacherAvailabilityError(
-                    `Teacher ${teacher.firstName} ${teacher.lastName} is not available for class ${newEvent.name}`
-                  );
-                }
-              }
+              await validateAvailabilities(teacher, intervals, newEvent.name);
             }
           }
         } catch (e) {

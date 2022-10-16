99892f92ccc6866ef40a079834876b2d6d711a31
Anshul-Birla <42627151+Anshul-Birla@users.noreply.github.com>
2022-10-15 22:39:27 -0700

    Added temp fix for firebase for staging route (#107)
    * hardcoded staging passwords
    
    * lint

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -5,15 +5,16 @@ const { privateKey } = JSON.parse(process.env.PRIVATE_KEY || "") || "";
 
 // eslint-disable @typescript-eslint/no-explicit-any
 const fbConfig = {
-  type: process.env.TYPE || "",
+  type: process.env.TYPE || "service_account",
   project_id: process.env.PROJECT_ID || "",
   private_key_id: process.env.PRIVATE_KEY_ID || "",
   private_key: privateKey || "",
   client_email: process.env.CLIENT_EMAIL || "",
   client_id: process.env.CLIENT_ID || "",
-  auth_uri: process.env.AUTH_URI || "",
-  token_uri: process.env.TOKEN_URI || "",
-  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL || "",
+  auth_uri: process.env.AUTH_URI || "https://accounts.google.com/o/oauth2/auth",
+  token_uri: process.env.TOKEN_URI || "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url:
+    process.env.AUTH_PROVIDER_X509_CERT_URL || "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url: process.env.CLIENT_x509_CERT_URL || "",
 };
 let fbAdmin;

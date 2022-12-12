56d388a6d38afaede7f5e024a71787db848e8fc5
Anshul Birla <anshulbirla@gmail.com>
2022-12-11 21:21:02 -0800

    temporar changes for remote dev

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,6 +1,7 @@
 import React, { createContext, useContext, useEffect, useMemo, useState } from "react";
 import "firebase/compat/auth";
 import firebase from "firebase/compat/app";
+import {connectAuthEmulator, getAuth} from "firebase/auth";
 import { FirebaseError } from "@firebase/util";
 import { Roles, UpdateUser, User } from "../models";
 import { APIContext } from "./APIContext";
@@ -81,11 +82,15 @@ export const AuthProvider: React.FC = ({ children }) => {
       apiKey: process.env.NEXT_PUBLIC_FB_API_KEY || "AIzaSyAx2FF4MDHl7p7p84Y_ZwvnKNxDSVN2dLw",
       authDomain:
         process.env.NEXT_PUBLIC_FB_AUTH_DOMAIN || "lap-student-tracker-staging.firebaseapp.com",
-      projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID || "lap-student-tracker-staging",
+      projectId: process.env.NEXT_PUBLIC_FB_PROJECT_ID || "demo-test",
       appId: process.env.NEXT_PUBLIC_FB_APP_ID || "1:289395861172:web:14d3154b0aed87f96f99e1",
     };
-    const app = firebase.apps[0] || firebase.initializeApp(fbConfig);
-    return app.auth();
+    const app = firebase.apps[0] || firebase.initializeApp(fbConfig)
+    const auth = app.auth();
+    // const app = firebase.apps[0] || firebase.initializeApp(fbConfig);
+    // const auth =  app.auth();
+    connectAuthEmulator(auth, "http://localhost:9099");
+    return auth;
   }, []);
 
   // get a new token and place it in the appropriate place
